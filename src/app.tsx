8f16b78711a4c362904552a7e0dfca60a89efef9
nahtanpng <nathan.ferreiira13@gmail.com>
2024-07-17 17:18:20 -0300

    feat: Remove Email from Invites

--- src/app.tsx
+++ src/app.tsx
@@ -54,6 +54,14 @@ export function App() {
     event.currentTarget.reset();
   }
 
+  function removeEmailFromInvites(emailToRemove: string) {
+    const newEmailList = emailsToInvite.filter(
+      (email) => email !== emailToRemove
+    );
+
+    setEmailsToInvite(newEmailList);
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
@@ -167,7 +175,12 @@ export function App() {
                     className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
                   >
                     <span className="text-zinc-300">{email}</span>
-                    <X className="size-4 text-zinc-300" />
+                    <button
+                      type="button"
+                      onClick={() => removeEmailFromInvites(email)}
+                    >
+                      <X className="size-4 text-zinc-300" />
+                    </button>
                   </div>
                 );
               })}
